bbc66bc046e5534bf6208cdc6f9e7b8cb1e7e1dd
Mandeep Singh <mandeep.enact@gmail.com>
2022-11-27 23:32:55 +0530

    Fixed opening of AlertActivity.kt on first Install of app when alarm trigger

---
+++ app/src/main/java/com/example/wakelockjavaapp/SingletonClass.kt
@@ -0,0 +1,10 @@
+package com.example.wakelockjavaapp
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+object SingletonClass {
+
+    val _trigger = MutableStateFlow(false)
+    val trigger : StateFlow<Boolean> = _trigger
+}
